ebb080888098e301522eafb36007af8107e86972
dyopa <aamood0821@gmail.com>
2024-07-05 01:48:07 +0900

    variable

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,44 @@
+/**
+ * Variable 선언하기
+ *
+ * 1) var - 잘 안써
+ * 2) let
+ * 3) const
+ */
+
+var name = '됴파';
+console.log(name);
+
+var age = 28;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경 가능
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '됴파';
+
+/**
+ * 선언과 할당
+ *
+ * 1) 변수를 선언하는 것
+ * 2) 할당
+ */
+
+var name = '됴파';
+console.log(name);
+
+let girlFriend;
+console.log('🚀 ~ girlFriend:', girlFriend);
+
+// const girlFriend2;
