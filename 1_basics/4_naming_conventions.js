5ddcb176f49f7dc179725d871f6a8ab7780ab979
dyopa <aamood0821@gmail.com>
2024-07-05 01:56:30 +0900

    naming

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,30 @@
+/**
+ * Name Conventions
+ *
+ * 변수이름 지을때
+ * 1)일반적으로 영어(라틴문자)를 사용하여 문자와 숫자를 모두 사용가능
+ * 2)특수기호는 _와 $만 가능
+ * 3)숫자로 이름을 시작할수없어
+ * 4)키워드는 변수명으로 사용할수없다
+ */
+
+let dyopa = '됴파';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log('🚀 ~ dyopa:', dyopa);
+console.log('🚀 ~ $ive:', $ive);
+console.log('🚀 ~ _yuJin:', _yuJin);
+
+/**
+ * Naming Convention
+ *
+ * 1) camelCase -> 띄어쓰기 다음 스펠링 대문자로 써주는거! 대부분 언어에서 쓴다!
+ * 2) snake_case -> 단어끼리 _ 로 묶는거 소문자로만 사용 (파이썬에서 많이 사용한대!)
+ * 3) PascalCase -> C# 마이크로소프트계열의 언어에서 사용 (첫번째도 대문자 띄어쓰기다음도 대문자)
+ *
+ * 1번 추천
+ */
+
+const anYuJin = '안유진';
+console.log('🚀 ~ anYuJin:', anYuJin);
